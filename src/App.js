e7faedf014293af586eb9e41bd2f86876f9f7afa
Arif KÖKEN <Arif KKEN>
2017-06-02 15:26:08 +0300

    auto publish sample

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ const App = () => (
   <div className="App">
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
+      <h2>React Uygulamasına Hoş Geldiniz.</h2>
     </div>
     <p className="App-intro">
-      To get started, edit yeni <code>src/App.js</code> and save to reload.
+      To get started, edit <code>src/App.js</code> and save to reload.
     </p>
   </div>
 );
